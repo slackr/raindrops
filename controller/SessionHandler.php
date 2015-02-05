d56a6a90028a214d92df98202c2ba0c0c3e694af
adrian <ap@slacknet>
2015-02-05 08:42:14 -0500

    read-only support for verify() and session_id sanity check

--- controller/SessionHandler.php
+++ controller/SessionHandler.php
@@ -23,37 +23,60 @@ class SessionHandler extends Object {
         $this->session_ip = $session_ip;
     }
 
-    public function verify() {
+    public function verify($read_only = false) {
         $seed = array($this->session_ip);
+        $is_ro = ($read_only ? '(read_only) ' : '');
 
         $this->id = null;
 
         if ($this->session_id != null) {
-            session_id($this->session_id);
+            if ($this->session_sanity_check()) {
+                session_id($this->session_id);
+            } else {
+                $this->log('Invalid session id ' . $this->session_tostring(), 3);
+                return false;
+            }
         }
 
         if (isset($_SESSION['rd_auth_identity'])) {
             $auth = new \Raindrops\Authentication($this->db, $_SESSION['rd_auth_identity'], $this->realm);
             if ($auth->verify_auth_token($_SESSION['rd_auth_token'], $seed)) {
-                $_SESSION['rd_auth_token'] = $auth->token; // update token to renew timestamp
+                if (! $read_only) {
+                    $_SESSION['rd_auth_token'] = $auth->token; // update token to renew timestamp
 
-				$this->id = new \Raindrops\Identity($this->db, $_SESSION['rd_auth_identity'], $this->realm);
-				if (! $this->id->get_identity()) {
-					$this->log('Failed to retrieve identity data: '. join('',$this->id->log_tail(1)), 3);
-                    return false;
-				}
+                    $this->id = new \Raindrops\Identity($this->db, $_SESSION['rd_auth_identity'], $this->realm);
+                    if (! $this->id->get_identity()) {
+                        $this->id = null;
+                        $this->log('Failed to retrieve identity data ' . $this->session_tostring() . ': '. join('',$this->id->log_tail(1)), 3);
+                        return false;
+                    }
+                }
             } else {
-				$this->id = null;
-                session_destroy();
-                session_start();
+                $this->id = null;
+                if (! $read_only) {
+                    session_destroy();
+                    session_start();
+                }
 
-                $this->log('Token verification failed: '. join('',$auth->log_tail(1)), 3);
+                $this->log($is_ro . 'Token verification failed ' . $this->session_tostring() . ': '. join('',$auth->log_tail(1)), 3);
                 return false;
             }
+
+            $this->log($is_ro . 'Session is valid ' . $this->session_tostring(), 0);
+            return true;
         }
 
-        $this->log('Session is valid', 0);
-        return true;
+        $this->log($is_ro . 'Session is invalid ' . $this->session_tostring(), 0);
+        return false;
+    }
+
+
+   function session_sanity_check() {
+       return preg_match('/^[a-z0-9]{26}$/i', $this->session_id);
+   }
+
+    public function session_tostring() {
+        return "(sid:". $this->session_id .",ip:". $this->session_ip .")";
     }
 }
 ?>
